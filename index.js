25da6e85599b5d450950188c879d340de222d35f
Brian <bgitau35@gmail.com>
2022-08-22 14:12:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+    {Year: "2020", result: "W"},
+    {Year: "2021", result: "L"},
+    {Year: "2022", result: "D"}
+]
+
+function superbowlWin (record) {
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+} 
